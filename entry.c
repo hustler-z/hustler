375fecea5ed1bb260ab0aef48382af531775ccd2
Hustler-X <roxhustlin@gmail.com>
2023-12-18 16:01:23 +0000

    Aarch64 assembly quick intro

---
+++ entry.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+
+/*
+ * ----------------------------------------------------
+ * The __asm keyword can incorporate inline GCC syntax
+ * assembly code into a function.
+ *
+ * ----------------------------------------------------
+ */
+
+#define barrier() __asm__ __volatile__("": : :"memory")
+
+extern int func(int a, int b);
+extern int info(void);
+extern int factorial(int x); 
+extern int pstate(void);
+
+unsigned int udiv(int a, int b)
+{
+  unsigned int ret;
+  __asm ("udiv %[res], %[div], %[fac]"
+         : [res] "=r"  (ret)
+         : [div] "r" (a), [fac] "r" (b)
+         : );
+  return ret;
+}
+
+int main(void)
+{
+  int a = 3, b = 13, x = 3;
+  info();
+
+  printf("sum = %d\n", func(a, b));
+  printf("PSTATE: 0x%016x\n", pstate());
+  printf("factorial of %d: %d\n", x, factorial(x));
+  printf("%d / %d = %d\n", b, a, udiv(b, a));
+  
+  return 0;
+}
+
