c1ffad6d985e16100a1a7cc87b469821ab5f4dbc
Hustler-X <roxhustlin@gmail.com>
2024-01-04 09:52:34 +0800

    modified the tab width of main.S & entry.c

--- entry.c
+++ entry.c
@@ -17,24 +17,24 @@ extern int pstate(void);
 
 unsigned int udiv(int a, int b)
 {
-  unsigned int ret;
-  __asm ("udiv %[res], %[div], %[fac]"
-         : [res] "=r"  (ret)
-         : [div] "r" (a), [fac] "r" (b)
-         : );
-  return ret;
+    unsigned int ret;
+    __asm ("udiv %[res], %[div], %[fac]"
+            : [res] "=r"  (ret)
+            : [div] "r" (a), [fac] "r" (b)
+            : );
+    return ret;
 }
 
 int main(void)
 {
-  int a = 3, b = 13, x = 3;
-  info();
+    int a = 3, b = 13, x = 3;
+    info();
 
-  printf("sum = %d\n", func(a, b));
-  printf("PSTATE: 0x%016x\n", pstate());
-  printf("factorial of %d: %d\n", x, factorial(x));
-  printf("%d / %d = %d\n", b, a, udiv(b, a));
+    printf("sum = %d\n", func(a, b));
+    printf("PSTATE: 0x%016x\n", pstate());
+    printf("factorial of %d: %d\n", x, factorial(x));
+    printf("%d / %d = %d\n", b, a, udiv(b, a));
   
-  return 0;
+    return 0;
 }
 
