eebed36338d375bdd922187aa604024e3623ca61
Hustler-X <roxhustlin@gmail.com>
2024-04-22 07:26:23 +0000

    Update project plan in 2024

--- os/docs/hustler.c
+++ os/docs/hustler.c
@@ -3,15 +3,19 @@
  * -----------------------------------------------------------------------------
  * My Goal for 2024
  *
- * LiteOS
- * Uniproton
+ * LiteOS         - RT kernel
+ * Uniproton      - RTOS
  * Linux
- * Xvisor
- * kvmtool
+ * Xvisor         - Type-1 Hypervisor
+ * kvmtool        - Type-2 Hypervisor
  *
+ * (1) Hypervisor Porting on radax-zero3w
  *
+ *              Linux + RTOS
+ *              ------------
+ *                 XVISOR
  *
- *
+ * (2) Algorithms
  *
  *
  *
