e240f99efb192ac9a6a2b1d2b5a3a11dd40af19a
Hustler-X <roxhustlin@gmail.com>
2024-04-23 16:58:52 +0000

    Add kvmtool to source, ignore last commit

---
+++ virtualization/vhustle-t2/cook.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+OPT=$1
+PLAT=$2
+
+usage() {
+printf "usage: ./cook.sh [options]
+options:
+    build             - build kvmtool
+    clean             - clean built objects
+"
+}
+
+build_kvmtool() {
+    make -j$(nproc)
+}
+
+build() {
+    echo "---------------------------------------------------------------------"
+    begin=$(date +%s)
+    build_kvmtool
+    end=$(date +%s)
+    tts=$(($end-$begin))
+    echo "Done build uboot in $(($tts/60)) min $(($tts%60)) sec"
+    echo "---------------------------------------------------------------------"
+}
+
+clean() {
+    make -j$(nproc) clean
+}
+
+main() {
+    case $OPT in
+        build)
+            build
+            ;;
+        clean)
+            clean
+            ;;
+        *)
+            usage
+            ;;
+    esac
+}
+
+main $1 $2
