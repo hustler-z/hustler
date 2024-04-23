a3bd9238a8aa8614b198fc6bce73fd850f0ff204
Hustler-X <roxhustlin@gmail.com>
2024-04-23 12:05:34 +0000

    Add sanbox build method to u-boot cook.sh

--- bootloader/u-boot/cook.sh
+++ bootloader/u-boot/cook.sh
@@ -1,22 +1,46 @@
 #!/bin/bash
 
 OPT=$1
+PLAT=$2
 
 usage() {
 printf "usage: ./cook.sh [options]
 options:
-    config - set up configuration of uboot
-    build  - build uboot
-    clean  - clean built objects
+    config            - set up configuration of uboot
+    build [platforms] - build uboot
+            qemu        uboot for QEMU
+            sandbox     uboot with trace enabled
+    clean             - clean built objects
 "
+}
+
+build_qemu() {
+    make -j$(nproc) qemu_arm64_defconfig
+    make -j$(nproc)
+}
+
+build_sandbox() {
+    make -j$(nproc) FTRACE=1 sandbox64_defconfig
+    make -j$(nproc) FTRACE=1
 }
 
 build() {
     # while CONFIG_SYS_TEXT_BASE=0x60000000
     echo "---------------------------------------------------------------------"
     begin=$(date +%s)
-    make -j$(nproc) qemu_arm64_defconfig
-    make -j$(nproc)
+    case $PLAT in
+        qemu)
+            build_qemu
+            ;;
+        sandbox)
+            build_sandbox
+            ;;
+        *)
+            usage
+            echo "---------------------------------------------------------------------"
+            exit
+            ;;
+    esac
     end=$(date +%s)
     tts=$(($end-$begin))
     echo "Done build uboot in $(($tts/60)) min $(($tts%60)) sec"
@@ -48,4 +72,4 @@ main() {
     esac
 }
 
-main $1
+main $1 $2
