da4feed045c3053521029e513583914656350656
Hustler-X <roxhustlin@gmail.com>
2024-04-23 03:28:11 +0000

    Modified uboot configs + add uboot cook.sh script for QEMU

---
+++ bootloader/u-boot/cook.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+OPT=$1
+
+usage() {
+printf "usage: ./cook.sh [options]
+options:
+    config - set up configuration of uboot
+    build  - build uboot
+    clean  - clean built objects
+"
+}
+
+build() {
+    # while CONFIG_SYS_TEXT_BASE=0x60000000
+    begin=$(date +%s)
+    make -j$(nproc) qemu_arm64_defconfig
+    echo "---------------------------------------------------------------------"
+    make -j$(nproc)
+    end=$(date +%s)
+    tts=$(($end-$begin))
+    echo "Done build uboot in $(($tts/60)) min $(($tts%60)) sec"
+    echo "---------------------------------------------------------------------"
+}
+
+config() {
+    make -j$(nproc) menuconfig
+}
+
+clean() {
+    make -j$(nproc) clean
+}
+
+main() {
+    case $OPT in
+        build)
+            build
+            ;;
+        clean)
+            clean
+            ;;
+        config)
+            config
+            ;;
+        *)
+            usage
+            ;;
+    esac
+}
+
+main $1
